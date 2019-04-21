7f903f981c4ee6ef8f024a282a9894bf59d73b36
Hoppy <hoppy@gnurdle.com>
2019-04-20 21:30:22 -0500

    few things to play with in the repl

--- src/clj101/core.clj
+++ src/clj101/core.clj
@@ -2,3 +2,11 @@
 
 (defn -main []
   (println "hello world!"))
+
+;; there's your hello world program.
+
+;; we'll play with a couple of things here, so you can get the 
+;; idea of how to play with the repl.
+
+;; step 1, use meta+n to set the namespace.  When you do that, the calva
+;; repl window will jump up
